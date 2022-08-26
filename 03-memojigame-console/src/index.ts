fd944f4f68ec8c38ce6c83630573ab7aeb869325
Paula Santamaría <m_paula_santamaria@live.com>
2022-08-25 21:01:35 -0300

    Simplify code

--- 03-memojigame-console/src/index.ts
+++ 03-memojigame-console/src/index.ts
@@ -9,98 +9,73 @@ type Tile = {
 // This will keep track of each tile's state.
 let tiles: Tile[] = [];
 
-/** Duplicate the initial emoji array (each tile has a pair) */
-function duplicate(emojis: string[]): string[] {
-    return [...emojis, ...emojis];
-}
-
-/** Shuffle emoji array so the tiles appear in different orders each time */
-function shuffle(emojis: string[]): string[] {
-    for (let i = 0; i < emojis.length; i++) {
-        const tile = emojis[i];
-        const randomNewPosition = Math.floor(Math.random() * emojis.length);
-
-        // Swap the current tile for the one in the new position.
-        emojis[i] = emojis[randomNewPosition]
-        emojis[randomNewPosition] = tile;
-    }
-
-    return emojis;
-}
-
 /** Create tile array from emoji array */
 function createTiles(emojis: string[]): Tile[] {
-    return emojis.map((e, index) => {
+    const duplicatedEmojis = [...emojis, ...emojis];
+
+    const shuffledEmojis = duplicatedEmojis;
+    for (let i = 0; i < shuffledEmojis.length; i++) {
+        const randomPos = Math.floor(Math.random() * (i + 1));
+        [shuffledEmojis[randomPos], shuffledEmojis[i]] = [shuffledEmojis[i], shuffledEmojis[randomPos]];
+    }
+
+    return shuffledEmojis.map((e, index) => {
         return {
             emoji: e,
-            state: 'idle',
+            state: 'flipped',
             index
         }
     });
 }
 
-function flip(tileIndex: number) {
-    const currentTile = tiles[tileIndex];
-    const flippedTiles = tiles.filter(t => t.state == 'flipped');
-
+function flip(tileIndex: number): void {
     // If the tile is already flipped, don't do anything.
-    if (flippedTiles.find(ft => ft.index == tileIndex))
+    if (tiles.find(ft => ft.index == tileIndex && (ft.state == 'cleared' || ft.state == 'flipped')))
         return;
 
+    const flippedTiles = tiles.filter(t => t.state == 'flipped');
+    const selectedTile = tiles[tileIndex];
+    selectedTile.state = 'flipped';
+
     if (flippedTiles.length == 2) {
+        // If 2 tiles are already flipped, face them back down.
         flippedTiles[0].state = 'idle';
         flippedTiles[1].state = 'idle';
-        currentTile.state = 'flipped';
     }
-    else if (flippedTiles.length == 1 && flippedTiles[0].emoji == currentTile.emoji) {
+    else if (flippedTiles.length == 1 && flippedTiles[0].emoji == selectedTile.emoji) {
+        // If 1 tile is flipped and it matches, clear them both.
         flippedTiles[0].state = 'cleared';
-        currentTile.state = 'cleared';
-    }
-    else {
-        currentTile.state = 'flipped';
-    }
-}
-
-function getTileCharacterByState(tile: Tile): string {
-    switch (tile.state) {
-        case 'idle': return tile.index.toString().padStart(2, ' ');              // Use this as example of dangers of inference?
-        case 'flipped': return tile.emoji;
-        case 'cleared': return '✅';
-        default: return 'Wrong state';
+        selectedTile.state = 'cleared';
     }
 }
 
 function printTiles(): void {
     console.clear();
+    const gridSize = 4;
 
-    let row = '';
-    let tilesClone = tiles.slice(0);
-
-    for (let rowIndex = 0; rowIndex < 4; rowIndex++) {
-        row = tilesClone.splice(0, 4)
-            .map(t => getTileCharacterByState(t))
+    for (var i = 0; i < tiles.length; i = i + gridSize) {
+        const row = tiles.slice(i, i + gridSize)
+            .map(tile => tile.state == 'idle' ? tile.index.toString().padStart(2, ' ') : tile.emoji)
             .join('  |  ');
 
         console.log(row);
-        row = '';
     }
 }
 
 function runGame() {
     printTiles();
+    readlineSync.question('Try to memorize the grid. Then, press Enter to continue.', { hideEchoBack: true, mask: '' });
 
-    let gameOver = false;
-    while (!gameOver) {
+    tiles.forEach(t => t.state = 'idle');
+    printTiles();
 
+    while (tiles.findIndex(t => t.state == 'idle') > -1) {
         let inputNumber = readlineSync.questionInt("Enter a tile's number to flip it: ", { min: 0, max: tiles.length });
         flip(inputNumber);
         printTiles();
     }
+    console.log('Game completed. Congrats!')
 }
 
-// Init game
-let processedEmojis = duplicate(['🎃', '🐶', '🐱', '🕹️', '⭐', '👾', '🍄', '🦴']);
-processedEmojis = shuffle(processedEmojis);
-tiles = createTiles(processedEmojis); // Set the initial state.
-
+tiles = createTiles(['🎃', '🐶', '🐱', '🎮', '⭐', '👾', '🍄', '🦴']); // Set the initial state.
 runGame();
