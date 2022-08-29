9326a491cb0c9d5312a336d70f6173a37ee96929
Paula Santamaría <m_paula_santamaria@live.com>
2022-08-29 20:33:27 -0300

    Change idle and flipped state by down and up

--- 03-memojigame-console/src/index.ts
+++ 03-memojigame-console/src/index.ts
@@ -16,13 +16,16 @@ function createTiles(emojis: string[]): Tile[] {
     const shuffledEmojis = duplicatedEmojis;
     for (let i = 0; i < shuffledEmojis.length; i++) {
         const randomPos = Math.floor(Math.random() * (i + 1));
-        [shuffledEmojis[randomPos], shuffledEmojis[i]] = [shuffledEmojis[i], shuffledEmojis[randomPos]];
+        const currentEmoji = shuffledEmojis[i];
+        // Swap [i] value with [randomPos] value.
+        shuffledEmojis[i] = shuffledEmojis[randomPos];
+        shuffledEmojis[randomPos] = currentEmoji;
     }
 
     return shuffledEmojis.map((e, index) => {
         return {
             emoji: e,
-            state: 'flipped',
+            state: 'up',
             index
         }
     });
@@ -30,17 +33,17 @@ function createTiles(emojis: string[]): Tile[] {
 
 function flip(tileIndex: number): void {
     // If the tile is already flipped or cleared, don't do anything.
-    if (tiles.find(ft => ft.index == tileIndex && ft.state != 'idle'))
+    if (tiles.find(ft => ft.index == tileIndex && ft.state != 'down'))
         return;
 
-    const flippedTiles = tiles.filter(t => t.state == 'flipped');
+    const flippedTiles = tiles.filter(t => t.state == 'up');
     const selectedTile = tiles[tileIndex];
-    selectedTile.state = 'flipped';
+    selectedTile.state = 'up';
 
     if (flippedTiles.length == 2) {
         // If 2 tiles are already flipped, face them back down.
-        flippedTiles[0].state = 'idle';
-        flippedTiles[1].state = 'idle';
+        flippedTiles[0].state = 'down';
+        flippedTiles[1].state = 'down';
     }
     else if (flippedTiles.length == 1 && flippedTiles[0].emoji == selectedTile.emoji) {
         // If 1 tile is flipped and it matches, clear them both.
@@ -56,7 +59,7 @@ function printTiles(): void {
     for (var i = 0; i < tiles.length; i = i + gridSize) {
         const row = tiles.slice(i, i + gridSize)
             .map(tile => {
-                if (tile.state == 'idle')
+                if (tile.state == 'down')
                     return tile.index.toString().padStart(2, ' ');
                 else
                     return tile.emoji;
@@ -69,12 +72,12 @@ function printTiles(): void {
 
 function runGame() {
     printTiles();
-    readlineSync.question('Try to memorize the grid. Then, press Enter to continue.');
+    readlineSync.question('Press Enter to start.');
 
-    tiles.forEach(t => t.state = 'idle');
+    tiles.forEach(t => t.state = 'down');
     printTiles();
 
-    while (tiles.findIndex(t => t.state == 'idle') > -1) {
+    while (tiles.findIndex(t => t.state == 'down') > -1) {
         let inputNumber = readlineSync.questionInt("Enter a tile's number: ");
         if (inputNumber >= 0 && inputNumber < 16) {
             flip(inputNumber);
