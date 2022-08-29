3f80e5ea4a846170dcb769b7d319de72e6fb7f32
Paula Santamaría <m_paula_santamaria@live.com>
2022-08-29 20:47:44 -0300

    Apply improvements done to original game

--- 03-memojigame-console-bonus/src/index.ts
+++ 03-memojigame-console-bonus/src/index.ts
@@ -20,13 +20,16 @@ class Game {
         const shuffledEmojis = duplicatedEmojis;
         for (let i = 0; i < shuffledEmojis.length; i++) {
             const randomPos = Math.floor(Math.random() * (i + 1));
-            [shuffledEmojis[randomPos], shuffledEmojis[i]] = [shuffledEmojis[i], shuffledEmojis[randomPos]];
+            const currentEmoji = shuffledEmojis[i];
+            // Swap [i] value with [randomPos] value.
+            shuffledEmojis[i] = shuffledEmojis[randomPos];
+            shuffledEmojis[randomPos] = currentEmoji;
         }
 
         return shuffledEmojis.map((e, index) => {
             return {
                 emoji: e,
-                state: 'flipped',
+                state: 'up',
                 index
             }
         });
@@ -34,17 +37,17 @@ class Game {
 
     flip(tileIndex: number): void {
         // If the tile is already flipped or cleared, don't do anything.
-        if (this.tiles.find(ft => ft.index == tileIndex && ft.state != 'idle'))
+        if (this.tiles.find(ft => ft.index == tileIndex && ft.state != 'down'))
             return;
-    
-        const flippedTiles = this.tiles.filter(t => t.state == 'flipped');
+
+        const flippedTiles = this.tiles.filter(t => t.state == 'up');
         const selectedTile = this.tiles[tileIndex];
-        selectedTile.state = 'flipped';
-    
+        selectedTile.state = 'up';
+
         if (flippedTiles.length == 2) {
             // If 2 tiles are already flipped, face them back down.
-            flippedTiles[0].state = 'idle';
-            flippedTiles[1].state = 'idle';
+            flippedTiles[0].state = 'down';
+            flippedTiles[1].state = 'down';
         }
         else if (flippedTiles.length == 1 && flippedTiles[0].emoji == selectedTile.emoji) {
             // If 1 tile is flipped and it matches, clear them both.
@@ -56,37 +59,40 @@ class Game {
     printTiles(): void {
         console.clear();
         const gridSize = 4;
-    
+
         for (var i = 0; i < this.tiles.length; i = i + gridSize) {
             const row = this.tiles.slice(i, i + gridSize)
                 .map(tile => {
-                    if (tile.state == 'idle')
+                    if (tile.state == 'down')
                         return tile.index.toString().padStart(2, ' ');
                     else
                         return tile.emoji;
                 })
                 .join('  |  ');
-    
+
             console.log(row);
         }
     }
 
     run() {
         this.printTiles();
-        readlineSync.question('Try to memorize the grid. Then, press Enter to continue.');
-    
-        this.tiles.forEach(t => t.state = 'idle');
+        readlineSync.question('Press Enter to start.');
+
+        this.tiles.forEach(t => t.state = 'down');
         this.printTiles();
-    
-        while (this.tiles.findIndex(t => t.state == 'idle') > -1) {
-            let inputNumber = readlineSync.questionInt("Enter a tile's number to flip it: ", { min: 0, max: this.tiles.length - 1 });
-            this.flip(inputNumber);
-            this.printTiles();
+
+        while (this.tiles.findIndex(t => t.state == 'down') > -1) {
+            let inputNumber = readlineSync.questionInt("Enter a tile's number: ");
+            if (inputNumber >= 0 && inputNumber < 16) {
+                this.flip(inputNumber);
+                this.printTiles();
+            }
+            else console.log('Invalid input');
         }
-        console.log('Game completed. Congrats!')
+        console.log('Game completed. Congrats!');
     }
 }
 
 // Now our state is handled by this instance.
-const game = new Game(['🎃', '🐶', '🐱', '🎮', '⭐', '👾', '🍄', '🦴']); 
+const game = new Game(['🎃', '🐶', '🐱', '🎮', '⭐', '👾', '🍄', '🦴']);
 game.run();
