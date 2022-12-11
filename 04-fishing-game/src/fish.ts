ec19764ad8c3567ab4e156237be590486cc289f3
Paula Santamaría <m.paula.santamaria@gmail.com>
2022-12-11 13:27:52 -0300

    Add fish logic

---
+++ 04-fishing-game/src/fish.ts
@@ -0,0 +1,86 @@
+export type Fish = {
+    name: string,
+    icon: string,
+    amount: number,
+    value: number,
+    probability: number,
+    element?: HTMLElement
+}
+
+const fishes: Fish[] = [
+    { name: 'boot', icon: '🥾', amount: 0, value: 1, probability: 4 },
+    { name: 'fish', icon: '🐟', amount: 0, value: 5, probability: 4 },
+    { name: 'tropical', icon: '🐠', amount: 0, value: 10, probability: 2 }
+]
+
+export function initialize() {
+    const fishesSection = document.getElementById('fish')!;
+
+    for (const fish of fishes) {
+        const container = document.createElement('div');
+        container.className = 'fish-item';
+
+        const amount = document.createElement('span');
+        amount.id = `${fish.name}-amount`;
+        const price = document.createElement('span');
+        price.id = `${fish.name}-price`;
+        price.innerHTML = ` x $${fish.value}`;
+
+        container.appendChild(amount);
+        container.appendChild(price);
+
+        fishesSection.appendChild(container);
+
+    }
+    const sellAllBtn = document.createElement('button');
+    sellAllBtn.innerHTML = 'Sell all';
+    sellAllBtn.onclick = sellAll;
+
+    fishesSection.appendChild(sellAllBtn);
+
+    update();
+}
+
+export function getRandomFish(): Fish | null {
+
+    // Distribute fish according to their probability.
+    const distribution = fishes.map((f, index) => {
+        const distributedIndex = [...Array<number>(f.probability).fill(index)];
+        return distributedIndex;
+    });
+
+    const flatDistribution = distribution.flat();
+
+    // Add -1 for missed attempts.
+    const missedAttemptsProbability =distribution.length;
+    flatDistribution.push(...Array<number>(missedAttemptsProbability).fill(-1));
+
+    // Get random fish from distribution.
+    const fishIndex = flatDistribution[Math.floor(Math.random() * flatDistribution.length)];
+
+    if (fishIndex == -1)
+        return null;
+
+    return fishes[fishIndex];
+}
+
+export function add(fish: Fish) {
+    fish.amount++;
+    update();
+}
+
+function update() {
+    for (const fish of fishes) {
+        const amount = document.getElementById(`${fish.name}-amount`)!;
+        amount.innerHTML = `${fish.icon} ${fish.amount}`
+    }
+}
+
+function sellAll() {
+    for (const fish of fishes) {
+        fish.amount = 0;
+    }
+
+    update();
+}
+
