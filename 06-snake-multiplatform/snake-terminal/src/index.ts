ed0302f50ddf218f09da69491390b786247378ee
Paula Santamaría <m.paula.santamaria@gmail.com>
2023-07-12 20:47:48 -0300

    render game

--- 06-snake-multiplatform/snake-terminal/src/index.ts
+++ 06-snake-multiplatform/snake-terminal/src/index.ts
@@ -1,3 +1,13 @@
-import { SnakeGame } from "snake-lib";
+import { SnakeGame, createSnake } from 'snake-lib';
+import { TerminalRenderer } from './terminal-renderer';
 
-const game = new SnakeGame();
+const game = new SnakeGame(30, 20, TerminalRenderer);
+const snake = createSnake(15, 10, 'right');
+game.add(snake.chunks);
+
+// Game loop.
+const timer = setInterval(() => {
+    game.update();
+    if (game.state == 'game over') 
+        clearInterval(timer);
+}, 300);
